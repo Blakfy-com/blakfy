131f07e46b5ce4225b5a723edf1b7528847c75ff
Tarık Tunç <tarktunc@hotmail.com>
2023-03-28 03:44:30 +0300

    navbarReklam

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import NavbarReklam from './Components/Navbar/NavbarReklam';
+
 function App() {
     return (
         <div className="App">
+            <NavbarReklam />
             <Navbar />
         </div>
     );
---
+++ src/Components/Navbar/NavbarReklam.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavbarReklam.css';
+
+function NavbarReklam() {
+    return (
+        <div className="NavTop">
+            <a href="#">Blakfy.com 1. YILA OZEL % 50 INDIRIM</a>
+        </div>
+    );
+}
+
+export default NavbarReklam;
