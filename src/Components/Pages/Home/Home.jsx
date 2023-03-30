6b2630c8b579d14feb3d8db32cd8b048b65c4987
Tarık Tunç <tarktunc@hotmail.com>
2023-03-30 03:19:21 +0300

    30020319

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navbar from '../../Navbar/Navbar';
 import Footer from '../../Footer/Footer';
+import Slider from './Slider/Slider';
 
 function Home() {
     return (
         <div>
             <Navbar />
             <h1>Burasi Home sayfasi</h1>
+
+            <Slider />
             <Footer />
         </div>
     );
---
+++ src/Components/Pages/Home/Slider/Slider.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Slider.css';
+
+function Slider() {
+    return (
+        <div>
+            <div className="slider-1">
+                <div>1/3</div>
+                <img src="../images/1-nolu-slider.webp" alt="sliderImage1" />
+                <div>Caption Text</div>
+            </div>
+        </div>
+    );
+}
+
+export default Slider;
