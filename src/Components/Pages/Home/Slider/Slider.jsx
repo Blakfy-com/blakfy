d5b575fb7f1c6e2699f80424612d7bde2986dbf7
Tarık Tunç <tarktunc@hotmail.com>
2023-04-02 01:20:17 +0300

    02040120

--- src/Components/Pages/Home/Slider/Slider.jsx
+++ src/Components/Pages/Home/Slider/Slider.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import './Slider.css';
 
+import { FiArrowLeft, FiArrowRight } from 'react-icons/fi';
+
 function Slider() {
     return (
         <div>
             <div className="sliderShow">
                 <div className="mySlides">
-                    <div className="numberText">1/3</div>
-                    <img src="/images/1-nolu-slider.webp" alt="sliderImage1" />
-                    <div className="text">Slider 1</div>
-                </div>
-                <div className="mySlides">
-                    <div className="numberText">2/3</div>
-                    <img src="/images/2-nolu-slider.webp" alt="sliderImage2" />
-                    <div className="text">Slider 2</div>
-                </div>
-                <div className="mySlides">
-                    <div className="numberText">3/3</div>
-                    <img src="/images/3-nolu-slider.webp" alt="sliderImage2" />
-                    <div className="text">Slider 3</div>
+                    <div className="sliderImage">
+                        <img
+                            src="/images/1-nolu-slider.webp"
+                            alt="sliderImage1"
+                        />
+                        <div className="prev">
+                            <FiArrowLeft />
+                        </div>
+                        <div className="next">
+                            <FiArrowRight />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
