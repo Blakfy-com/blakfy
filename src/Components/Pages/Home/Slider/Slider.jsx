263e50cc35fb325f787f0c49a7b0e8fecaeef508
Tarık Tunç <tarktunc@hotmail.com>
2023-04-01 21:39:34 +0300

    01042139

--- src/Components/Pages/Home/Slider/Slider.jsx
+++ src/Components/Pages/Home/Slider/Slider.jsx
@@ -4,10 +4,22 @@ import './Slider.css';
 function Slider() {
     return (
         <div>
-            <div className="slider-1">
-                <div>1/3</div>
-                <img src="../images/1-nolu-slider.webp" alt="sliderImage1" />
-                <div>Caption Text</div>
+            <div className="sliderShow">
+                <div className="mySlides">
+                    <div className="numberText">1/3</div>
+                    <img src="/images/1-nolu-slider.webp" alt="sliderImage1" />
+                    <div className="text">Slider 1</div>
+                </div>
+                <div className="mySlides">
+                    <div className="numberText">2/3</div>
+                    <img src="/images/2-nolu-slider.webp" alt="sliderImage2" />
+                    <div className="text">Slider 2</div>
+                </div>
+                <div className="mySlides">
+                    <div className="numberText">3/3</div>
+                    <img src="/images/3-nolu-slider.webp" alt="sliderImage2" />
+                    <div className="text">Slider 3</div>
+                </div>
             </div>
         </div>
     );
