2b57d537b70c8598f56cccf137a3fa14aa2e0b07
Tarık Tunç <tarktunc@hotmail.com>
2023-04-03 00:59:52 +0300

    project update

---
+++ src/Components/Pages/Home/Projects/Projects.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Projects.css';
+
+function Projects() {
+    return (
+        <section class="section">
+            <div class="companies">
+                <div class="company">
+                    <li class="companyImage">
+                        <img src="/images/CompaniesLogo/Birincioglu.webp" />
+                    </li>
+                </div>
+                <div class="company">
+                    <li class="companyImage">
+                        <img src="/images/CompaniesLogo/Galatahukuk.webp" />
+                    </li>
+                </div>
+                <div class="company">
+                    <li class="companyImage">
+                        <img src="/images/CompaniesLogo/Tripslab.webp" />
+                    </li>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Projects;
