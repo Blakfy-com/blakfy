e08181c1e9ae27aba4b291abba501bf5b43c6889
Tarık Tunç <tarktunc@hotmail.com>
2023-04-03 19:24:29 +0300

    PackageAdvantage v1

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -3,14 +3,15 @@ import Navbar from '../../Navbar/Navbar';
 import Footer from '../../Footer/Footer';
 import Slider from './Slider/Slider';
 import Projects from './Projects/Projects';
+import PackageAdvantage from './PackageAdvantage/PackageAdvantage';
 
 function Home() {
     return (
         <div>
             <Navbar />
-            <h1>Burasi Home sayfasi</h1>
             <Slider />
             <Projects />
+            <PackageAdvantage />
             <Footer />
         </div>
     );
---
+++ src/Components/Pages/Home/PackageAdvantage/PackageAdvantage.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import './PackageAdvantage.css';
+import { BsSearch } from 'react-icons/bs';
+
+function PackageAdvantage() {
+    return (
+        <div>
+            <div className="container">
+                <div className="rows">
+                    <div className="modulTittle">
+                        <h1>Türkiye'nin en gelişmiş e-ticaret yazılımı</h1>
+                        <p>
+                            En büyük e-ticaret sitelerinin kullandığı,
+                            Türkiye'nin en iyi e-ticaret alt yapısı ile zaman
+                            kaybetmeden başarıyı yakalayın.
+                            <br />
+                            Benzersiz kapsamda, 7/24 teknik destek, Kampanya
+                            Kurguları, Pazarlama Modülleri ve Mobil Uygulamalar
+                            ile fark yaratın!
+                        </p>
+                    </div>
+                </div>
+                <div className="rows rowTwo">
+                    <div className="modul">
+                        <div className="anasayfaItem">
+                            <p className="logo">
+                                <i>
+                                    <BsSearch />
+                                </i>
+                            </p>
+                            <p className="title">
+                                <strong>Gelismis ve esnek</strong>
+                            </p>
+                            <p className="comment">
+                                Benzersiz bulut e-ticaret altyapısı ile
+                                trendleri zaman ve para kaybetmeden yakalayın.
+                                Satışlarınızı arttırın ve operasyonel
+                                yüklerinizi hafifletin.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="modul">
+                        <div className="anasayfaItem">
+                            <p className="logo">
+                                <i>
+                                    <BsSearch />
+                                </i>
+                            </p>
+                            <p className="title">
+                                <strong>Gelismis ve esnek</strong>
+                            </p>
+                            <p className="comment">
+                                Benzersiz bulut e-ticaret altyapısı ile
+                                trendleri zaman ve para kaybetmeden yakalayın.
+                                Satışlarınızı arttırın ve operasyonel
+                                yüklerinizi hafifletin.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="modul">
+                        <div className="anasayfaItem">
+                            <p className="logo">
+                                <i>
+                                    <BsSearch />
+                                </i>
+                            </p>
+                            <p className="title">
+                                <strong>Gelismis ve esnek</strong>
+                            </p>
+                            <p className="comment">
+                                Benzersiz bulut e-ticaret altyapısı ile
+                                trendleri zaman ve para kaybetmeden yakalayın.
+                                Satışlarınızı arttırın ve operasyonel
+                                yüklerinizi hafifletin.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="modul">
+                        <div className="anasayfaItem">
+                            <p className="logo">
+                                <i>
+                                    <BsSearch />
+                                </i>
+                            </p>
+                            <p className="title">
+                                <strong>Gelismis ve esnek</strong>
+                            </p>
+                            <p className="comment">
+                                Benzersiz bulut e-ticaret altyapısı ile
+                                trendleri zaman ve para kaybetmeden yakalayın.
+                                Satışlarınızı arttırın ve operasyonel
+                                yüklerinizi hafifletin.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PackageAdvantage;
