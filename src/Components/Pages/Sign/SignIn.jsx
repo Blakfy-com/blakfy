caa7ad5795f226ad20700f11fe28f6da2133621a
Henkerr <118183620+Henkerr@users.noreply.github.com>
2023-04-10 23:54:19 +0300

    SignIn update

--- src/Components/Pages/Sign/SignIn.jsx
+++ src/Components/Pages/Sign/SignIn.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./SignIn.css";
 
-function SignIn() {
+export const SignIn = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
     return (
-        <div>
-            <h2>Burasi SignIn sayfasi</h2>
+    
+        <div className="auth-form-container">
+            <h1>Welcome Back!</h1>
+            <h2>Sign In</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">Email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="password">Password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <button type="submit">Sing In</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
         </div>
-    );
+    )
 }
-
 export default SignIn;
+
+
